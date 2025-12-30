cd015517359ef53450260f68f9e08d5dca5085ef
iampaswan <krishna1119kumar@gmail.com>
2025-12-30 21:02:17 +0530

    eight commit

--- RagChatbotFrotend/src/apiconfig/api.tsx
+++ RagChatbotFrotend/src/apiconfig/api.tsx
@@ -1,15 +1,26 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4000';
+// const url = 'http://localhost:4000';
+const url = import.meta.env.VITE_API_URL;
 
 export const getRagResponse = (question: string) => {
-  return axios.post(`${url}`, { question });
+  return axios.post(`${url}/`, { question });
 }
 
 export const getChats = () => {
   return axios.get(`${url}/chatHistory`);
 }
 
-export const uploading = (formData: any) => {
-  return axios.post(`${url}/upload`, formData);
-}
+// export const uploading = (formData: any) => {
+//   return axios.post(`${url}/upload`, formData);
+// }
+
+
+
+export const uploading = (formData: FormData) => {
+  return axios.post(`${url}/upload`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
